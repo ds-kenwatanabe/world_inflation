3e8feae615d77b25c61c0af80235e386ab1a78eb
chris <ollhoffchristian@gmail.com>
2024-06-06 13:11:12 -0300

    Corrected line chart display

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 
-
 class InflationApp:
     # Function to establish a connection to the MySQL database using secrets
     def get_connection(self):
@@ -39,6 +38,10 @@ class InflationApp:
         # Convert the result into a DataFrame for easier handling
         df = pd.DataFrame(inflation)
 
+        # Ensure numeric columns are correctly typed
+        df['average_inflation'] = pd.to_numeric(df['average_inflation'], errors='coerce')
+        df['annual_inflation'] = pd.to_numeric(df['annual_inflation'], errors='coerce')
+
         st.subheader("Venezuela and Argentina excluded")
         st.write(df)
 
@@ -57,10 +60,17 @@ class InflationApp:
             inflation_line = self.run_query(line_query)
             df_line = pd.DataFrame(inflation_line)
 
+            # Ensure numeric columns are correctly typed
+            # It might change decimal to nominal
+            df_line['year'] = pd.to_numeric(df_line['year'], errors='coerce')
+            df_line['average_inflation'] = pd.to_numeric(df_line['average_inflation'], errors='coerce')
+
             st.subheader(f"Inflation Data for {selected_country}")
             st.write(df_line)
-            # Line chart
-            st.line_chart(df_line, x="year", y="average_inflation", color="#FF0000")
+
+            # Create the line chart
+            st.line_chart(df_line.set_index('year')['average_inflation'],
+                          color='#ff0000', use_container_width=True)
 
 
 if __name__ == '__main__':
